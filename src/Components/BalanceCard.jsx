136c723f3c69652374b5a24fd31d51504cc8645a
Gideon Oluwole <oluwolegideon00@gmail.com>
2025-04-17 17:38:40 +0100

    Implemented show/hide balance and some minor ui tweaks

--- src/Components/BalanceCard.jsx
+++ src/Components/BalanceCard.jsx
@@ -1,8 +1,17 @@
-const BalanceCard = ({ balanceTitle, balanceAmount, balanceCurrency, cryptocurrency }) => {
-  
-  const name = `${8}`
+import { useState } from "react";
+import { HiEye, HiEyeSlash } from "react-icons/hi2";
+
+const BalanceCard = ({ 
+  balanceTitle, 
+  balanceAmount, 
+  balanceCurrency, 
+  cryptocurrency,
+  showBalance,
+  toggleShowBalance}) => {
+
   return (
     <div className="dashboard_balance-card">
+      {/* <img className="dashboard_balance-background" src="src/assets/balancebackground.svg" alt="" /> */}
       <div className="dashboard_crypto-container">
         <div className="dashboard_balance-title">{balanceTitle}</div>
         <div className="dashboard_cryptocurrency">
@@ -10,8 +19,12 @@ const BalanceCard = ({ balanceTitle, balanceAmount, balanceCurrency, cryptocurre
           {cryptocurrency}
         </div>
       </div>
-      <div className="dashboard_balance-amount">{balanceAmount}
-        <span className="dashboard_balance-currency">{balanceCurrency}</span>
+      <div className="dashboard_balance-amount">
+        {balanceAmount}
+        <div className="dashboard_balance-currency">{balanceCurrency}</div>
+        <div className="dashboard_show-balance" onClick={toggleShowBalance}>
+        {showBalance ? <HiEye className="balance-icon"/> : <HiEyeSlash className="balance-icon"/> }
+        </div>
       </div>  
     </div>
   )
--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,45 +1,74 @@
-import BalanceCard from '../Components/BalanceCard';
+import { useState } from 'react';
 import { HiArrowDownTray, HiArrowUpTray } from 'react-icons/hi2'
-import '../Dashboard/Dashboard.css';
+import BalanceCard from '../Components/BalanceCard';
 import RecentActivity from './RecentActivity';
 import QuickLinks from './QuickLinks';
+import '../Dashboard/Dashboard.css';
 
 const Dashboard = () => {
+  const [availableBalance, setAvailableBalance] = useState('120,000')
+  const [totalDeposit, setTotalDeposit] = useState('52,000')
+  const [totalWithdrawn, setTotalWithdrawn] = useState('0')
+  const [balanceCurrency, setBalanceCurrency] = useState('PHP')
+  const [cryptocurrency, setCryptocurrency] = useState('USDT')
+  const [showBalance, setShowBalance] = useState(true)
+
+  const toggleShowBalance = () => {
+    if (showBalance) {
+      setShowBalance(!showBalance)
+      setAvailableBalance('*****')
+      setTotalDeposit('*****')
+      setTotalWithdrawn('*****')
+
+    } else {
+      setShowBalance(true)
+      setAvailableBalance('120,000')
+      setTotalDeposit('52,000')
+      setTotalWithdrawn('0')
+    }
+  }
+
   return (
     <div className='dashboard_container'>
       <div className="dashboard_title-container">
         <div className="dashboard_greeting">
-          Welcome <span className="dashboard_username"> Gideon 👋</span>
-          <div className="dashboard_welcome-remark">
-            How are you doing today?
-          </div>
+          Welcome 
+          <div className="dashboard_username"> Gideon </div>
           <div className="dashboard_account-tier">
-            Trading Account:
-            Tier 1
+            TIER 1
           </div>
         </div>
+          <div className="dashboard_welcome-remark">
+            How are you doing today? 👋
+          </div>
       </div>
       <div className="dashboard_balance-container">
         <BalanceCard 
-        cryptocurrency={"BTC"}
+        cryptocurrency={cryptocurrency}
         balanceTitle={"Available Balance"}
-        balanceAmount={"120,000"}
-        balanceCurrency={" USD "}/>
+        balanceAmount={availableBalance}
+        balanceCurrency={balanceCurrency}
+        showBalance={showBalance}
+        toggleShowBalance={toggleShowBalance}/>
         
         <BalanceCard
-        cryptocurrency={"BTC"} 
+        cryptocurrency={cryptocurrency} 
         balanceTitle={"Total Deposit"} 
-        balanceAmount={"52,000"}
-        balanceCurrency={" USD "}/>
+        balanceAmount={totalDeposit}
+        balanceCurrency={balanceCurrency}
+        showBalance={showBalance}
+        toggleShowBalance={toggleShowBalance}/>
         
         <BalanceCard 
-        cryptocurrency={"BTC"}
+        cryptocurrency={cryptocurrency}
         balanceTitle={"Total Withdrawn"}
-        balanceAmount={0}
-        balanceCurrency={" USD "}/>
+        balanceAmount={totalWithdrawn}
+        balanceCurrency={balanceCurrency}
+        showBalance={showBalance}
+        toggleShowBalance={toggleShowBalance}/>
       </div>
       <QuickLinks />
-      <RecentActivity cryptocurrency="BTC"/>
+      <RecentActivity cryptocurrency={cryptocurrency}/>
     </div>
   )
 }
