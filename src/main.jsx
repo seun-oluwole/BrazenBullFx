3a3a13a3d5a60467acbe36d2d6660e820c2debd8
Gideon Oluwole <oluwolegideon00@gmail.com>
2025-08-17 00:28:19 +0100

    new fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { createRoot } from "react-dom/client";
-import { AuthContextProvider } from "../src/context/AuthContext";
+import { AuthContextProvider } from './context/AuthContext';
 import { Toaster } from "react-hot-toast";
-import WalletContextProvider from "../src/context/WalletContextProvider";
-import AdminContextProvider from "../src/context/AdminContext";
-import ModalContextProvider from "../src/context/modalContext";
-import App from "./App/App";
+import WalletContextProvider from './context/WalletContextProvider';
+import AdminContextProvider from './context/AdminContext';
+import ModalContextProvider from './context/modalContext';
+import App from './App/App';
 
 createRoot(document.getElementById("root")).render(
   <AuthContextProvider>
@@ -15,22 +15,10 @@ createRoot(document.getElementById("root")).render(
           <Toaster 
             containerClassName="toastContainer"
             toastOptions={{
-              success: {
-                style: {
-                  padding: '8px'
-                }
-
-              },
-              error: {
-                style: {
-                background: '#ff2323',
-                color: '#fff',
-                padding: '8px'
-              }
-              },
-
-          
-            }}/>
+              success: { style: { padding: '8px' } },
+              error: { style: { background: '#ff2323', color: '#fff', padding: '8px' } },
+            }}
+          />
         </ModalContextProvider>
       </AdminContextProvider>
     </WalletContextProvider>
